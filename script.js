956a68cbeea1981c9548b0e3d4302b9e3fe353d2
asaulters <71015650+asaulters@users.noreply.github.com>
2021-06-06 21:17:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//Nav
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+        // console.log(index /7);
+      }
+    });
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
