18a4d6b92066f92dff320383d1154e9855ea165b
asaulters <71015650+asaulters@users.noreply.github.com>
2021-06-10 17:33:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// document vars
+let step1WU = document.querySelector(".home-HIWWUS1");
+let step2WU = document.querySelector(".home-HIWWUS2");
+let step3WU = document.querySelector(".home-HIWWUS3");
+let step4WU = document.querySelector(".home-HIWWUS4");
+let step1Img = document.querySelector(".home-HIWImg1");
+let step2Img = document.querySelector(".home-HIWImg2");
+let step3Img = document.querySelector(".home-HIWImg3");
+let step4Img = document.querySelector(".home-HIWImg4");
+
 //Nav
 const navSlide = () => {
   const burger = document.querySelector(".burger");
@@ -23,3 +33,62 @@ const navSlide = () => {
   });
 };
 navSlide();
+
+//How it Works Scroller
+function HIW1() {
+  //display none current img
+  step2Img.style.display = "none";
+  step3Img.style.display = "none";
+  step4Img.style.display = "none";
+  //change HIW img to this step
+  step1Img.style.display = "block";
+  //onclick toggle competed and highlight left border on clicked
+  step1WU.classList.toggle("on");
+  step2WU.classList.remove("on");
+  step3WU.classList.remove("on");
+  step4WU.classList.remove("on");
+}
+function HIW2() {
+  //display none current img
+  step1Img.style.display = "none";
+  step3Img.style.display = "none";
+  step4Img.style.display = "none";
+  //change HIW img to this step
+  step2Img.style.display = "block";
+  //onclick toggle competed and highlight left border on clicked
+  step2WU.classList.toggle("on");
+  step1WU.classList.remove("on");
+  step3WU.classList.remove("on");
+  step4WU.classList.remove("on");
+}
+function HIW3() {
+  //display none current img
+  step1Img.style.display = "none";
+  step2Img.style.display = "none";
+  step4Img.style.display = "none";
+  //change HIW img to this step
+  step3Img.style.display = "block";
+  //onclick toggle competed and highlight left border on clicked
+  step3WU.classList.toggle("on");
+  step2WU.classList.remove("on");
+  step1WU.classList.remove("on");
+  step4WU.classList.remove("on");
+}
+function HIW4() {
+  //display none current img
+  step1Img.style.display = "none";
+  step2Img.style.display = "none";
+  step3Img.style.display = "none";
+  //change HIW img to this step
+  step4Img.style.display = "block";
+  //onclick toggle competed and highlight left border on clicked
+  step4WU.classList.toggle("on");
+  step2WU.classList.remove("on");
+  step3WU.classList.remove("on");
+  step1WU.classList.remove("on");
+}
+
+step1WU.addEventListener("click", HIW1);
+step2WU.addEventListener("click", HIW2);
+step3WU.addEventListener("click", HIW3);
+step4WU.addEventListener("click", HIW4);
